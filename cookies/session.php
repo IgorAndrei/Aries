8a91140fd8d06c48e6ba621f55afc9e7f47b3b4f
SporeGroup <igor.riquelme@gmail.com>
2015-10-15 14:33:45 -0300

    destrucción sesión 30 minutos
    Aries

--- cookies/session.php
+++ cookies/session.php
@@ -4,7 +4,14 @@ $connection = mysql_connect("localhost", "root", "");
 // Selecting Database
 $db = mysql_select_db("sga", $connection);
 mysql_query("SET NAMES 'utf8'");
-session_start();// Starting Session
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
